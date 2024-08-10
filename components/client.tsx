1d396de90daf9c0b673fdac560e8d515632200f1
gaurav5xy <gauravkumar81464@gmail.com>
2024-08-10 11:36:35 +0530

    "auth"

---
+++ components/client.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from 'react'
+import Server from './server';
+
+const Client = () => {
+    console.log("Client component");
+    
+  return (
+    <>
+    <div>This an example client component</div>
+    {/* <Server/> */}
+    </>
+  )
+}
+
+export default Client
---
+++ components/server.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Server = () => {
+    console.log("Server component");
+    
+  return (
+    <div>This an example server component</div>
+  )
+}
+
+export default Server
