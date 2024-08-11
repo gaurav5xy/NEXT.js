a2324382c0036f28dab84bfe73b7f553a4453315
gaurav5xy <gauravkumar81464@gmail.com>
2024-08-11 10:43:42 +0530

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,22 @@ import {
 } from '@clerk/nextjs'
 import './globals.css'
 
+import {Nunito} from 'next/font/google';
+import { Metadata } from 'next';
+
+const nunito = Nunito({
+  subsets: ['latin'],
+  weight: ['1000','200','300','400','500','600','700','800','900'],
+  variable: '--font-nunito'
+})
+export const metadata: Metadata = {
+  title: 'DevFlow',
+  description: 'A collaborative platform for developers to share knowledge and solve coding challenges.',
+  icons: {
+    icon: '/favicon.ico',
+  }
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -15,7 +31,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>
+        <body className={`${nunito.variable}`}>
           {/* <SignedOut>
             <SignInButton />
           </SignedOut> */}
